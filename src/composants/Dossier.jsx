872c2a78256d57a6fc833fc2a4d892d3015722fc
Lyna <lyly8888@hotmail.com>
2021-03-20 18:57:05 -0400

    point C complete

--- src/composants/Dossier.jsx
+++ src/composants/Dossier.jsx
@@ -2,8 +2,22 @@ import './Dossier.scss';
 import { IconButton } from '@material-ui/core';
 import SortIcon from '@material-ui/icons/Sort';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import React from 'react';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 export default function Dossier({id, nom, couleur, datemodif, couverture}) {
+
+  // lorsque je chenge le com de anchorEl a ancrageEle, le menu ne s'affiche pas a la bonne place
+  const [anchorEl, setAncrageEle] = React.useState(null);
+
+  const gererCli = (event) => {
+    setAncrageEle(event.currentTarget);
+  };
+
+  const gererFermer = () => {
+    setAncrageEle(null);
+  };
 
   return (
     <article className="Dossier" style={{backgroundColor: couleur}}>
@@ -17,9 +31,19 @@ export default function Dossier({id, nom, couleur, datemodif, couverture}) {
         <h2>{nom}</h2>
         <p>Modifié : {formaterDate(datemodif)}</p>
       </div>
-      <IconButton className="modifier" aria-label="modifier" size="small">
+      <IconButton aria-controls="simple-menu" aria-haspopup="true" onClick={gererCli} className="modifier" aria-label="modifier" size="small">
         <MoreVertIcon />
       </IconButton>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={gererFermer}
+      >
+        <MenuItem onClick={gererFermer}>Modifier</MenuItem>
+        <MenuItem onClick={gererFermer}>Supprimer</MenuItem>
+      </Menu>
     </article>
   );
 }
